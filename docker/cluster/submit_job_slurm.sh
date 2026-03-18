f3ba508f426e70a03f6c62203eeab8ffbfe74e96
jkamohara3 <jkamohara3@gatech.edu>
2026-03-18 16:26:01 -0400

    update batch job script

--- docker/cluster/submit_job_slurm.sh
+++ docker/cluster/submit_job_slurm.sh
@@ -1,20 +1,39 @@
 #!/usr/bin/env bash
 
-# in the case you need to load specific modules on the cluster, add them here
-# e.g., `module load eth_proxy`
+# Phoenix
+# cat <<EOT > job.sh
+# #!/bin/bash
 
-# create job script with compute demands
-### MODIFY HERE FOR YOUR JOB ###
+# #SBATCH --account=gts-yzhao301
+# #SBATCH --cpus-per-task=12
+# #SBATCH --gpus-per-node=rtx_pro_6000_blackwell:1
+# #SBATCH --mem-per-gpu=24G
+# #SBATCH --time=2:00:00
+# #SBATCH --nodes=1
+# #SBATCH -qinferno
+# #SBATCH --mail-type=BEGIN,END,FAIL
+# #SBATCH --mail-user=jkamohara3@gatech.edu
+# #SBATCH --job-name="training-$(date +"%Y-%m-%dT%H:%M")"
+
+# # Pass the container profile first to run_singularity.sh, then all arguments intended for the executed script
+# bash "$1/docker/cluster/run_singularity.sh" "$1" "$2" "${@:3}"
+# EOT
+
+# sbatch < job.sh
+# rm job.sh
+
+# ICE
 cat <<EOT > job.sh
 #!/bin/bash
 
-#SBATCH -n 1
-#SBATCH --cpus-per-task=8
-#SBATCH --gpus=rtx_3090:1
-#SBATCH --time=23:00:00
-#SBATCH --mem-per-cpu=4048
-#SBATCH --mail-type=END
-#SBATCH --mail-user=name@mail
+#SBATCH --cpus-per-task=12
+#SBATCH --gpus-per-node=rtx_pro_6000_blackwell:1
+#SBATCH --mem-per-gpu=24G
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH -q=coe-grade
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=jkamohara3@gatech.edu
 #SBATCH --job-name="training-$(date +"%Y-%m-%dT%H:%M")"
 
 # Pass the container profile first to run_singularity.sh, then all arguments intended for the executed script
