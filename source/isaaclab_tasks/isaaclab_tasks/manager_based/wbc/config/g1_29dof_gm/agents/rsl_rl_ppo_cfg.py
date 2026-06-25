30f54eb3831471e1952bfeb6928480a9fd3b02b5
Andy Zong <andyzong@Andys-MacBook-Pro.local>
2026-06-24 18:04:33 -0700

    Lower PPO learning rate to 1e-4 for G1 GM

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/wbc/config/g1_29dof_gm/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/wbc/config/g1_29dof_gm/agents/rsl_rl_ppo_cfg.py
@@ -39,7 +39,7 @@ class G1PPORunnerCfg(RslRlOnPolicyRunnerCfg):
         entropy_coef=0.005,
         num_learning_epochs=5,
         num_mini_batches=4,
-        learning_rate=1.0e-3,
+        learning_rate=1.0e-4,
         schedule="adaptive",
         gamma=0.99,
         lam=0.95,
